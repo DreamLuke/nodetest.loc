87113c8d566dd6e275fa8df6d82c2a006123e5c3
DreamLuke <luke1048@rambler.ru>
2019-04-10 21:23:34 +0300

    Проект после выполнения команды php artissan:make auth

---
+++ resources/views/start.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div>
+            <example-component>555</example-component>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/start', function () {
+    return view('start');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
