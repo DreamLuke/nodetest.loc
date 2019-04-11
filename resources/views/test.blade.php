c38f8a4a531c7fed314044275f5bc6360d12e68b
DreamLuke <luke1048@rambler.ru>
2019-04-11 10:33:53 +0300

    Внёс изменения в test.blade.php (убрал привязку к макету app.blade.php, подключил ссылки на css и js в файле)

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<script src="{{ asset('js/app.js') }}" defer></script>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-@section('content')
+<div id="app">
     <example-component></example-component>
-
-@endsection
+</div>
